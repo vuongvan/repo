046f2df76b0af96a35b0bd75c7371acf109bfb12
Fei Yu <maivanvuong@gmail.com>
2026-02-24 03:02:49 +0700

    Update 345movie.net.js

--- repo/345movie.net.js
+++ repo/345movie.net.js
@@ -11,6 +11,7 @@
 // @nsfw         false
 // @tags         anime, phim, vietsub
 // ==/MiruExtension==
+
 export default class extends Extension {
   async load() {
     this.registerSetting({
@@ -21,52 +22,55 @@ export default class extends Extension {
       defaultValue: "https://phimapi.com",
     });
   }
+
   async latest(page) {
     const apiDomain = await this.getSetting("api_domain");
     const res = await this.request("", {
-      headers: { "Miru-Url": `${apiDomain}/danh-sach/phim-moi-cap-nhat?page=${page}` },
+      headers: { "Miru-Url": apiDomain + "/danh-sach/phim-moi-cap-nhat?page=" + page },
     });
     const data = JSON.parse(res);
     return data.items.map((item) => ({
       title: item.name,
       url: item.slug,
       cover: item.poster_url,
-      update: item.year ? `Năm ${item.year}` : ""
     }));
   }
+
   async search(kw, page) {
     const apiDomain = await this.getSetting("api_domain");
     const res = await this.request("", {
-      headers: { "Miru-Url": `${apiDomain}/v1/api/tim-kiem?keyword=${kw}&limit=20&page=${page}` },
+      headers: { "Miru-Url": apiDomain + "/v1/api/tim-kiem?keyword=" + kw + "&limit=20&page=" + page },
     });
     const data = JSON.parse(res);
     return data.data.items.map((item) => ({
       title: item.name,
       url: item.slug,
-      cover: `https://phimimg.com/${item.poster_url}`,
+      cover: "https://phimimg.com/" + item.poster_url,
     }));
   }
+
   async detail(url) {
     const apiDomain = await this.getSetting("api_domain");
     const res = await this.request("", {
-      headers: { "Miru-Url": `${apiDomain}/phim/${url}` },
+      headers: { "Miru-Url": apiDomain + "/phim/" + url },
     });
     const data = JSON.parse(res);
     const movie = data.movie;
     const episodes = data.episodes.map((server) => ({
       title: server.server_name,
       urls: server.server_data.map((item) => ({
-        name: `Tập ${item.name}`,
+        name: "Tập " + item.name,
         url: item.link_m3u8,
       })),
     }));
     return {
       title: movie.name,
       cover: movie.poster_url,
-      desc: movie.content ? movie.content.replace(/<[^>]*>?/gm, '') : "Không có mô tả.",
+      desc: movie.content ? movie.content.replace(/<[^>]*>?/gm, "") : "",
       episodes: episodes,
     };
   }
+
   async watch(url) {
     return {
       type: "hls",
